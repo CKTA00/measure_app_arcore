323b29a5b90a341da47b281ece1d9e78c2aaae49
JarKolod <jkolodziej58@outlook.com>
2024-04-20 21:00:48 +0200

    ui setup

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] PlacePoint placePoint;
+
+
+    public void OnPointPlaceBtnPress()
+    {
+        placePoint.PlaceAtCenter();
+    }
+
+    public void OnPointRemoveBtnPress()
+    {
+
+    }
+
+    public void On90DegressBtnPress()
+    {
+
+    }
+}
